355f1a9587944497929fe1077168362bcb44008e
Himanshu Kumar singh <78970009+s1997himanshu@users.noreply.github.com>
2021-04-01 12:49:56 +0530

    Add files via upload

---
+++ Automatic pull up bike stand_project.ino
@@ -0,0 +1,47 @@
+int M11=12,M12=13,S1=5;
+int a=0;
+void setup()
+{
+  // put your setup code here, to run once:
+pinMode(M11,OUTPUT);
+pinMode(M12,OUTPUT);
+pinMode(S1,INPUT);
+digitalWrite(M11,LOW);
+digitalWrite(M12,LOW);
+delay(1000);
+// put your main code here, to run repeatedly:
+  a=digitalRead(S1);
+  delay(20000);
+  if(a==LOW);
+  {
+
+digitalWrite(M11,LOW);
+digitalWrite(M12,HIGH);
+//digitalWrite(M21,LOW);
+//digitalWrite(M22,LOW);
+delay(50);
+
+  } 
+  if(a==LOW);
+ {
+  digitalWrite(M11,LOW);
+  digitalWrite(M12,LOW);
+  delay(20000);
+ 
+ if(a==HIGH);
+
+digitalWrite(M11,HIGH);
+digitalWrite(M12,LOW);
+delay(50);
+if(a==HIGH);
+
+digitalWrite(M11,LOW);
+digitalWrite(M12,LOW);
+}
+}
+
+ 
+
+void loop() {
+}
+
